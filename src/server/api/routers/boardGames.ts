def996fb49661087165b7f9a0aaf2b32d5bb8192
moses-codes <moses.ilagan@gmail.com>
2023-10-10 11:43:20 -0700

    Mechanics Table now Linked to Game Table

--- src/server/api/routers/boardGames.ts
+++ src/server/api/routers/boardGames.ts
@@ -7,53 +7,92 @@ import {
 } from "~/server/api/trpc";
 
 const bgInfoSchema = z.object({
+    title: z.string(),
     playTime: z.number(),
     minPlayers: z.number(),
     maxPlayers: z.number(),
     complexity: z.number(),
     image: z.string(),
-    gameId: z.number(),
+    id: z.number(),
 });
 
 const bgMechanicsSchema = z.array(
     z.object({
-        mechanicId: z.number(),
+        id: z.number(),
         mechanicText: z.string(),
     })
 );
 
 export const boardGamesRouter = createTRPCRouter({
 
-    addGame: publicProcedure
+    addGame: protectedProcedure
         .input(z.object({
             bgInfo: bgInfoSchema,
             bgMechanics: bgMechanicsSchema,
         }))
         .mutation(async ({ ctx, input }) => {
-            //console.log(input)
-            // input.bgMechanics.forEach(mechanic => {
-            //     mechanic.mechanicId = mechanic.mechanicId
-            //     const addMechanics = ctx.db.mechanic.create({
-            //         data: {
-            //             id: mechanic.mechanicId,
-            //             mechanicText: mechanic.mechanicText,
-            //             games: {
-            //                 create: {
 
-            //                 }
-            //             }
-            //         }
-            //     })
-            // })
-            console.log('mee when i', input.bgMechanics, input.bgInfo)
+            const { bgInfo, bgMechanics } = input
+
+            // data: {
+            //     email: 'vlad@prisma.io',
+            //     posts: {
+            //       connect: [{ id: 8 }, { id: 9 }, { id: 10 }],
+            //     },
+            //   },
+
+            //needs an array of mechanics with each id required.
+            const bgMechanicsIds = bgMechanics.map(mechanic => {
+                return {
+                    id: mechanic.id
+                }
+            })
+
+            console.log(bgMechanicsIds)
+
+            //add the mechanics into DB first
+            const addMechanics = await ctx.db.mechanic.createMany({
+                data: [...input.bgMechanics],
+                skipDuplicates: true,
+            })
+
+            const addGame = await ctx.db.game.create({
+                data: {
+                    id: bgInfo.id,
+                    title: bgInfo.title,
+                    minPlayers: bgInfo.minPlayers,
+                    maxPlayers: bgInfo.maxPlayers,
+                    complexity: bgInfo.complexity,
+                    image: bgInfo.image,
+                    playTime: bgInfo.playTime,
+                    mechanics: {
+                        connect: [...bgMechanicsIds]
+                    },
+
+                    // posts: {
+                    //     createMany: {
+                    //       data: [{ title: 'My first post' }, { title: 'My second post' }],
+                    //     },
+                    //   },
+                },
+            })
+
+            // model Game {
+            //     id         String     @id @unique
+            //     title      String
+            //     image      String?
+            //     playTime   Int
+            //     maxPlayers Int
+            //     minPlayers Int
+            //     complexity Float
+            //     user       User[]
+            //     mechanics  Mechanic[]
+            //     // gameSessions GameSession[]
+            // }
+
+            //connect the mechanics
+
+            console.log(bgInfo)
         }),
 
-    // addMechanics: publicProcedure
-    //     .input(z.array(z.object({ mechanicId: z.number(), mechanicText: z.string() })))
-    //     .mutation(({ input }) => {
-    //         console.log(input)
-    //         return {
-    //             greeting: input,
-    //         }
-    //     })
 });
