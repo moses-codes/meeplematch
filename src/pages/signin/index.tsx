aa940c2510ff88e0f3ee4413b80b8efddf400125
moses-codes <moses.ilagan@gmail.com>
2024-04-22 12:47:57 -0700

    testing authfix

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -53,7 +53,7 @@ export default function SignIn({ providers }: { providers: CustomPageProps }) {
                                     key={provider.id}
                                     onClick={() => {
                                         void signIn(provider.id, {
-                                            callbackUrl: baseUrl + provider.callbackUrl,
+                                            callbackUrl: `${window.location.origin}/shelf`,
                                         });
                                     }}
                                 >
