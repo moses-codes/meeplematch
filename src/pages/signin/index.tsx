59d85fcf2f2331a9cbb969b015e8e94131552a0c
moses-codes <moses.ilagan@gmail.com>
2024-04-22 10:36:36 -0700

    re-built

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -50,7 +50,7 @@ export default function SignIn({ providers }: { providers: CustomPageProps }) {
                                 key={provider.id}
                                 onClick={() => {
                                     void signIn(provider.id, {
-                                        callbackUrl: `/shelf`
+                                        callbackUrl: `${provider.callbackUrl}`,
                                     });
                                 }}
                             >
